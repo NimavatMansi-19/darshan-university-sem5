21fb6fa9d0d65b8e1b318fbf6562f1e1bd493de2
Nimavat Mansi <144536348+NimavatMansi-19@users.noreply.github.com>
2025-05-31 08:17:19 +0530

    Add files via upload

---
+++ TA WT/Lab11/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const fac=["Arjun Bala","Chirag Sir","Dharmik Sir"]
+  return (
+    fac.map(temp=><p>{temp}</p>)
+  );
+}
+
+export default App;
